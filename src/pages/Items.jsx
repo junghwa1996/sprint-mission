0f6918e1277e36038ed3b5857c9294d087d01075
junghwa yang <junghwa1996@naver.com>
2024-10-10 02:39:27 +0900

    페이지 나누지않고 데이터 같이 사용

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,11 +1,32 @@
-import ProductAllArea from "../components/Items/ProductAllArea";
-import ProductBestArea from "../components/Items/ProductBestArea";
+import { useEffect, useState } from "react";
+import DropDown from "../components/Items/DropDown";
+import ProductList from "../components/Items/ProductList";
+import SearchInput from "../components/Items/SearchInput";
+import { getProductList } from "../service/productAPI";
 
 function Items() {
+  const [productList, setProductList] = useState([]);
+
+  const handleLoad = async () => {
+    const { list } = await getProductList();
+    setProductList(list);
+  };
+
+  useEffect(() => {
+    handleLoad();
+  }, []);
+
   return (
     <>
-      <ProductBestArea />
-      <ProductAllArea />
+      <h2>베스트 상품</h2>
+      <ProductList list={productList} />
+      <div>
+        <h2>전체 상품</h2>
+        <SearchInput />
+        <a href="undefined">상품 등록하기</a>
+        <DropDown />
+      </div>
+      <ProductList list={productList} />
     </>
   );
 }
